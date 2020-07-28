dcc6112737db88a25b32574dc8b38b9966d55427
Hector Gonzalez <hectarek13@gmail.com>
2020-07-28 10:39:24 -0400

    minor fix

--- src/main/java/com/tts/techtalenttwitter/config/SecurityConfiguration.java
+++ src/main/java/com/tts/techtalenttwitter/config/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.tts.techtalenttwitter.config;
 
-import javax.activation.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
