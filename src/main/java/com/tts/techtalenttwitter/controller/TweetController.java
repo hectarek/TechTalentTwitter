09607a69e133aa713fcabc593c23aef86622f6ef
Hector Gonzalez <hectarek13@gmail.com>
2020-07-29 09:24:52 -0400

    added hashtags and following

--- src/main/java/com/tts/techtalenttwitter/controller/TweetController.java
+++ src/main/java/com/tts/techtalenttwitter/controller/TweetController.java
@@ -4,17 +4,18 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import com.tts.techtalenttwitter.model.Tweet;
+import com.tts.techtalenttwitter.model.User;
+import com.tts.techtalenttwitter.service.TweetService;
+import com.tts.techtalenttwitter.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import com.tts.techtalenttwitter.model.Tweet;
-import com.tts.techtalenttwitter.model.User;
-import com.tts.techtalenttwitter.service.TweetService;
-import com.tts.techtalenttwitter.service.UserService;
 
 @Controller
 public class TweetController {
@@ -47,5 +48,13 @@ public class TweetController {
             model.addAttribute("tweet", new Tweet());
         }
         return "newTweet";
+    }
+    
+    @GetMapping(value = "/tweets/{tag}")
+    public String getTweetsByTag(@PathVariable(value="tag") String tag, Model model) {
+        List<Tweet> tweets = tweetService.findAllWithTag(tag);
+        model.addAttribute("tweetList", tweets);
+        model.addAttribute("tag", tag);
+        return "taggedTweets";
     }
 }
